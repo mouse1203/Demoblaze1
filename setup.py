25dccde54d90d528bd52dd5fa5dcaf7a7eb9d0b9
mouse1203 <iakovenko.oksana@gmail.com>
2023-05-15 00:16:50 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Demoblaze',
+    version='1.0.0',
+    packages=['your_package'],
+    install_requires=[
+        'pytest >= 7.3.1',
+        'pytest-asyncio >= 0.21.0',
+        'pytest-playwright >=  0.3.3',
+        'pytest-reporter >=  0.5.2',
+        'pytest-reporter-html1 >= 0.8.2',
+        'playwright >= 1.33.0',
+    ],
+)    
